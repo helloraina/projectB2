4c5173156598e0bd6a925c09de0733feac52e0e9
helloraina <helloraina@nexon.co.kr>
2019-04-18 17:32:23 +0900

    domui-생성 및 스타일 옵션 적용 함수로 정리

--- js/domUI.js
+++ js/domUI.js
@@ -1,143 +1,157 @@
   class DomUI {
-    constructor(domClass, domID) {
-
-      // ui type === modal -> class - flex_modal
-      this.domID = domID;
-      this.domClass = domClass;
-
-      // var dom = document.createElement('div');
-      // dom.className = 'modalWrapper';
-      // dom.innerHTML = this.render();
-
-      // container.appendChild(dom);
-    }
-    //modal인 경우 -
-
-    render() {
-      return `
-        <div class="${this.domClass}" id="${this.domID}">
-          <div class="${this.domClass}-top">
-              <span class="left"></span>
-              <span class="center _width"></span>
-              <span class="right"></span>
-          </div>
-          <div class="${this.domClass}-middle _height">
-              <span class="left"></span>
-              <span class="center _width"></span>
-              <span class="right"></span>
-          </div>
-          <div class="${this.domClass}-bottom">
-              <span class="left"></span>
-              <span class="center _width"></span>
-              <span class="right"></span>
-          </div>
-        </div>`;
-    }
-
-    render2() {
-      return `
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-          <div class="pixel"></div>
-       `;
-    }
-
-    show() {
-      document.getElementById(this.domID).style.display = 'block';
-      container.classList.add('show');
-
-      this.addCloseBtn();
-    }
-
-    hide() {
-      document.getElementById(this.domID).style.display = 'none';
-
-      // document.getElementById(this.domID).parentNode.remove(this);
-      container.classList.remove('show');
-    }
-
-    addButtonEvent(value, type) {
-      const button = document.createElement('button');
-      const target = document.getElementById(this.domID);
+    constructor() {
+      const game = document.getElementById('Game');
       
-      target.querySelector('.flex_modal-bottom').querySelector('.center').appendChild(button);
+      const domContainer = document.createElement('div');
+      domContainer.className = 'uiContainer';
 
-      button.innerHTML = value;
-      button.classList.add('button');
-      button.classList.add('s1');
+      this.container = domContainer;
+      this.screenWidth = game.offsetWidth;
+      this.screenHeight = game.offsetHeight;
+    }
+
+    createBaseDom() {
+      const domContainer = document.createElement('div');
+      domContainer.className = 'uiContainer';
+
+      document.body.append(domContainer);
+
+      this.container = domContainer;
+    }
+
+    removeBaseDom() {
+      document.body.removeChild(this.container);
+    }
+
+    showModal(name) {
+      this.createBaseDom();
+
+      var modal = new DomModal(this.container, 400, 300);
       
-      console.dir(button);
+      this.addChild(modal.dom);
+      modal.addTitle(name);
+      this.modal = modal.dom;
 
-      button.addEventListener('click', function () {
-        console.log('close');
-        let modalID = target.parentNode.querySelector('div').id;
-        document.getElementById(modalID).style.display = 'none';
-        container.classList.remove('show');
-      });
-    }
-
-
-    addCloseBtn (){
-      const target = document.getElementById(this.domID);
-
-      const closeBtn = document.createElement('button');
-      closeBtn.className = 'closeBtn';
+      var btn = new DomButton('Okay');
+      btn.setPosition('bottom', 'right');
+      modal.dom.appendChild(btn.dom);
       
-      target.querySelector('.flex_modal-top').appendChild(closeBtn);
-    }
-    showTestModal() {
-      document.createAttribute
-    }
-
-    setTitle(value) {
-      const title = document.createElement('h2');
-      title.id = "title";
-      const modal = document.getElementById(this.domID);
-      
-      if(modal.querySelector('#title') !== null ) {
-        return false;
-      } 
-
-      modal.querySelector('.flex_modal-top').querySelector('.center').appendChild(title);
-
-      console.log('value ' + value);
-      title.innerHTML = value;
+      setTimeout(() => {
+        this.modal.style.transform = 'translate(135%, 10%)';
+      }, 100);
     }
 
-    resize(_width, _height, _offsetX) {
-      // 팝업을 여러개 띄울 때 위치 조정이 필요함..임시 
-      if (_offsetX === null) {
-        _offsetX = 1;
-      }
-
-      var dd = document.getElementById(this.domID);
-      
-      dd.style.marginLeft = (-_width * 0.5 * _offsetX) + 'px';
-      dd.getElementsByClassName('_height')[0].style.height = _height + 'px';
-
-      var resizeWid = dd.getElementsByClassName('_width');
-
-      Array.prototype.filter.call(resizeWid, function (resizeWid) {
-        return resizeWid.style.width = _width + 'px';
-      });
+    hideModal(){
+      this.removeBaseDom();
     }
 
-
-    showpixel() {
-      const pixelBox = document.createElement('div');
-      pixelBox.id = "pixelBox";
-      container.appendChild(pixelBox);
-      pixelBox.innerHTML = this.render2();
-    }
-
-    pixelresize(value) {
-      const pixelBox = document.getElementById('div');
-      pixelBox.getElementsByClassName('pixel').style.width = value + 'px';
+    addChild(_dom) {
+      this.container.appendChild(_dom);
     }
   }
+
+class DomModal extends DomUI {
+  constructor (container, _width, _height) {
+    super();
+    var domModal = document.createElement('div');
+
+    domModal.innerHTML = this.render();
+    domModal.classList.add('pixelBox');
+    domModal.classList.add('hasCloseBtn');// 삭제 시 닫기버튼 감춤.
+
+    domModal.style.width = _width + 'px';
+    domModal.style.height = _height + 'px';
+    
+    // modal background image path 변경
+    var path = "modal2";
+
+    for(let i = 0; i < domModal.children.length; i++) {
+      var pixel = domModal.children[i];
+
+      pixel.style.backgroundImage = `url('/assets/ui/${path}/nineBox_0${i+1}.png')`;
+      
+      if( pixel.classList.contains('_width')) {
+        pixel.style.width = (_width - 26)  + 'px';
+      } 
+      if ( pixel.classList.contains('_height') ) {
+        pixel.style.height = (_height - 26)  + 'px';
+      }
+    }
+
+    const closeBtn = document.createElement('button');
+    closeBtn.className = 'closeBtn';
+    domModal.appendChild(closeBtn);
+
+    this.removeDom (closeBtn);
+
+    this.dom = domModal;
+    console.dir(domModal);
+  }
+
+  addTitle(text) {
+    const title = document.createElement('h2');
+    title.classList.add('title');
+    title.innerText = text;
+    this.dom.appendChild(title);
+  }
+
+  removeDom(target) {
+    target.addEventListener('click', function() {
+      target.parentNode.parentNode.removeChild(target.parentNode);
+    });
+  }
+
+  render() {
+    return `
+        <div class="pixel"></div>
+        <div class="pixel _width"></div>
+        <div class="pixel"></div>
+        <div class="pixel _height"></div>
+        <div class="pixel _width _height"></div>
+        <div class="pixel _height"></div>
+        <div class="pixel"></div>
+        <div class="pixel _width"></div>
+        <div class="pixel"></div>
+     `;
+  }
+}
+
+class DomButton extends DomUI {
+  constructor (value, type) {
+    super();
+
+    const button = document.createElement('button');
+    button.innerHTML = value;
+    button.className = 'button';
+
+    if( type !== null ) {
+      button.classList.add(type); 
+    }
+    this.dom = button;
+  }
+
+  setPosition(_vertical, _align) {
+    // _vertical: top, bottom
+    // _align: left, center, right
+
+    this.dom.classList.add('__' + _vertical);
+    this.dom.classList.add('__' + _align);
+  }
+}
+
+class DomInventory extends DomModal {
+  constructor () {
+    const contentBox = document.createElement('div');
+    this.addChild(contentBox);
+    this.content = contentBox;
+  }
+  updateItems() {
+    const _colum = 5;
+    const _row = 3;
+
+    itemsWrap.style.className = 'inventoryContents';
+
+    const item = document.createElement('div');
+
+  }
+}
